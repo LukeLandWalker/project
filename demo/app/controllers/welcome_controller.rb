03f8d1dd1acb19e59ef911f203636dc18312d6b5
LukeLandWalker <wang5422726@126.com>
2014-04-27 16:12:46 +0800

    Multi-row submit

--- demo/app/controllers/welcome_controller.rb
+++ demo/app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
     def say
     end
+	def new
+	end
 end
---
+++ surveysay/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveysay::Application.config.secret_token = '49ed13acfd42579513889a1885982aa048a5279b0de81064c7f17d57033387a3012c0bf292ac7097455405285d76aa62a904612061130a5f7ad621c087e399a9'
